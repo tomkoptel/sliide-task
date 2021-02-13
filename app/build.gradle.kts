5ee52c6059b3e5548f174ebcef1c906f8272fc96
Tom Koptel <tom.koptel@gmail.com>
2021-02-13 16:12:53 +0100

    Remove ident config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,81 +1,81 @@
 plugins {
-  id("com.android.application")
-  id("kotlin-android")
+    id("com.android.application")
+    id("kotlin-android")
 }
 
 android {
-  compileSdkVersion(30)
-  buildToolsVersion("30.0.2")
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.2")
 
-  defaultConfig {
-    applicationId("com.olderwold.sliide")
-    minSdkVersion(21)
-    targetSdkVersion(30)
-    versionCode = 1
-    versionName = "1.0"
+    defaultConfig {
+        applicationId("com.olderwold.sliide")
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
 
-    testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
-  }
-
-  buildTypes {
-    getByName("release") {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_1_8.toString()
-  }
-  testOptions {
-    unitTests.isReturnDefaultValues = true
-    unitTests.isIncludeAndroidResources = true
-  }
-  lintOptions {
-    lintConfig = file("lint.xml")
-    textReport = true
-    textOutput("stdout")
-  }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+        unitTests.isIncludeAndroidResources = true
+    }
+    lintOptions {
+        lintConfig = file("lint.xml")
+        textReport = true
+        textOutput("stdout")
+    }
 }
 
 dependencies {
-  // Align project versions
-  implementation(platform(project(":platform")))
-  // Align versions of all Kotlin components
-  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-  // Use the Kotlin JDK 8 standard library.
-  implementation(kotlin("kotlin-stdlib-jdk8"))
+    // Align project versions
+    implementation(platform(project(":platform")))
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    // Use the Kotlin JDK 8 standard library.
+    implementation(kotlin("kotlin-stdlib-jdk8"))
 
-  implementation("androidx.core:core-ktx")
-  implementation("androidx.appcompat:appcompat")
-  implementation("androidx.constraintlayout:constraintlayout")
+    implementation("androidx.core:core-ktx")
+    implementation("androidx.appcompat:appcompat")
+    implementation("androidx.constraintlayout:constraintlayout")
 
-  implementation("com.google.android.material:material")
+    implementation("com.google.android.material:material")
 
-  implementation("com.squareup.okhttp3:okhttp")
-  implementation("com.squareup.okhttp3:logging-interceptor")
-  implementation("com.squareup.retrofit2:retrofit")
-  implementation("com.squareup.retrofit2:converter-gson")
-  implementation("com.squareup.retrofit2:adapter-rxjava2")
-  implementation("com.google.code.gson:gson")
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("com.squareup.retrofit2:retrofit")
+    implementation("com.squareup.retrofit2:converter-gson")
+    implementation("com.squareup.retrofit2:adapter-rxjava2")
+    implementation("com.google.code.gson:gson")
 
-  implementation("io.reactivex.rxjava2:rxjava")
-  implementation("io.reactivex.rxjava2:rxandroid")
-  implementation("com.github.pwittchen:reactivenetwork-rx2")
+    implementation("io.reactivex.rxjava2:rxjava")
+    implementation("io.reactivex.rxjava2:rxandroid")
+    implementation("com.github.pwittchen:reactivenetwork-rx2")
 
-  testImplementation("junit:junit")
-  testImplementation("org.amshove.kluent:kluent")
-  testImplementation("io.mockk:mockk")
-  testImplementation("io.mockk:mockk-dsl")
-  testImplementation("com.airbnb.okreplay:okreplay")
-  testImplementation("com.airbnb.okreplay:junit")
+    testImplementation("junit:junit")
+    testImplementation("org.amshove.kluent:kluent")
+    testImplementation("io.mockk:mockk")
+    testImplementation("io.mockk:mockk-dsl")
+    testImplementation("com.airbnb.okreplay:okreplay")
+    testImplementation("com.airbnb.okreplay:junit")
 
-  androidTestImplementation("androidx.test.ext:junit")
-  androidTestImplementation("androidx.test.espresso:espresso-core")
+    androidTestImplementation("androidx.test.ext:junit")
+    androidTestImplementation("androidx.test.espresso:espresso-core")
 }
--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -1,43 +1,43 @@
 plugins {
-  id("java-platform")
+    id("java-platform")
 }
 
 dependencies {
-  constraints {
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
-    api("androidx.core:core-ktx:1.3.2")
-    api("androidx.appcompat:appcompat:1.2.0")
-    api("androidx.constraintlayout:constraintlayout:2.0.4")
+    constraints {
+        api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+        api("androidx.core:core-ktx:1.3.2")
+        api("androidx.appcompat:appcompat:1.2.0")
+        api("androidx.constraintlayout:constraintlayout:2.0.4")
 
-    api("com.google.android.material:material:1.3.0")
+        api("com.google.android.material:material:1.3.0")
 
-    val okhttpVersion = "4.4.1"
-    api("com.squareup.okhttp3:okhttp:$okhttpVersion")
-    api("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
+        val okhttpVersion = "4.4.1"
+        api("com.squareup.okhttp3:okhttp:$okhttpVersion")
+        api("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
-    val retrofitVersion = "2.9.0"
-    api("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    api("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    api("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
-    api("com.google.code.gson:gson:2.8.6")
+        val retrofitVersion = "2.9.0"
+        api("com.squareup.retrofit2:retrofit:$retrofitVersion")
+        api("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+        api("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
+        api("com.google.code.gson:gson:2.8.6")
 
-    api("junit:junit:4.13.1")
+        api("junit:junit:4.13.1")
 
-    val mockkVersion = "1.9.3"
-    api("io.mockk:mockk:$mockkVersion")
-    api("io.mockk:mockk-dsl:$mockkVersion")
+        val mockkVersion = "1.9.3"
+        api("io.mockk:mockk:$mockkVersion")
+        api("io.mockk:mockk-dsl:$mockkVersion")
 
-    api("org.amshove.kluent:kluent:1.61")
+        api("org.amshove.kluent:kluent:1.61")
 
-    val okreplayVersion = "1.6.0"
-    api("com.airbnb.okreplay:okreplay:$okreplayVersion")
-    api("com.airbnb.okreplay:junit:$okreplayVersion")
+        val okreplayVersion = "1.6.0"
+        api("com.airbnb.okreplay:okreplay:$okreplayVersion")
+        api("com.airbnb.okreplay:junit:$okreplayVersion")
 
-    api("androidx.test.ext:junit:1.1.2")
-    api("androidx.test.espresso:espresso-core:3.3.0")
+        api("androidx.test.ext:junit:1.1.2")
+        api("androidx.test.espresso:espresso-core:3.3.0")
 
-    api("io.reactivex.rxjava2:rxjava:2.2.21")
-    api("io.reactivex.rxjava2:rxandroid:2.1.1")
-    api("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
-  }
+        api("io.reactivex.rxjava2:rxjava:2.2.21")
+        api("io.reactivex.rxjava2:rxandroid:2.1.1")
+        api("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
+    }
 }
